c0a5baa1064b020eb317e6ab07807bd333e0f8f8
Niko Hellgren <niko.hellgren@gmail.com>
2020-12-02 20:59:33 +0200

    Add day 1

---
+++ src/bin/day01.rs
@@ -0,0 +1,76 @@
+// Task: Find the two entries that sum up to 2020 and return their product.
+// Second task: Find three numbers with the same condition.
+mod helpers;
+
+fn main() {
+    let filename: &str = "day01.txt";
+    let input = helpers::input_helpers::read_input(&filename)
+        .unwrap()
+        .iter()
+        .map(|r| r.parse::<u32>().unwrap())
+        .collect();
+
+    let result = find_multiple_of_pair_with_sum(&input, 2020);
+
+    match result {
+        Some(answer) => println!("Task 1: {}", answer),
+        None => println!("Task 1: No product found!"),
+    }
+
+    let result = find_multiple_of_triplet_with_sum(&input, 2020);
+
+    match result {
+        Some(answer) => println!("Task 2: {}", answer),
+        None => println!("Task 2: No product found!"),
+    }
+}
+
+fn find_multiple_of_pair_with_sum(input: &Vec<u32>, wanted_sum: u32) -> Option<u32> {
+    for val1 in input {
+        for val2 in input {
+            if val1 + val2 == wanted_sum {
+                return Some(val1 * val2);
+            }
+        }
+    }
+
+    None
+}
+
+fn find_multiple_of_triplet_with_sum(input: &Vec<u32>, wanted_sum: u32) -> Option<u32> {
+    for val1 in input {
+        for val2 in input {
+            for val3 in input {
+                if val1 + val2 +val3 == wanted_sum {
+                    return Some(val1 * val2 * val3);
+                }
+            }
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::find_multiple_of_pair_with_sum;
+    use crate::find_multiple_of_triplet_with_sum;
+
+    #[test]
+    fn verify_example_task_1() {
+        let result = find_multiple_of_pair_with_sum(&vec![1721, 979, 366, 299, 675, 1456], 2020);
+        match result {
+            Some(product) => assert_eq!(514579, product),
+            None => panic!(),
+        }
+    }
+
+    #[test]
+    fn verify_example_task_2() {
+        let result = find_multiple_of_triplet_with_sum(&vec![1721, 979, 366, 299, 675, 1456], 2020);
+        match result {
+            Some(product) => assert_eq!(241861950, product),
+            None => panic!(),
+        }
+    }
+}
---
+++ src/bin/helpers/input_helpers.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+pub fn read_input(input_name: &str) -> Result<Vec<String>, std::io::Error> {
+    let mut input_path = String::from("input/");
+    input_path.push_str(input_name);
+
+    let reader = BufReader::new(File::open(input_path)?);
+    return reader.lines().collect();
+}
+
